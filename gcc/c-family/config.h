7e731826f06b5f32c0ad901705407f22a2ffadbf
James Michael DuPont <JamesMikeDuPont@gmail.com>
2015-10-12 12:18:44 -0400

    fake headers to compile without running configure.

---
+++ gcc/c-family/config.h
@@ -0,0 +1,90 @@
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>// memcpy
+void *memcpy(void *dest, const void *src, size_t n);
+//#define HOST_BITS_PER_WIDE_INT 32
+//#define HOST_WIDE_INT long
+#define CHAR_BIT 8
+# define CPPCHAR_SIGNED_T int
+//#define ENUM_BITFIELD(TYPE) enum TYPE
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 8
+#define CONST_MODE_SIZE
+#define CONST_MODE_IBIT
+#define CONST_MODE_FBIT
+const int NUM_MACHINE_MODES = 1;
+
+#define CONST_MODE_BASE_ALIGN
+
+enum machine_mode 
+  {
+  };
+#define MAX_MODE_FLOAT 2
+#define MIN_MODE_FLOAT 1
+
+#define MAX_MODE_DECIMAL_FLOAT 2
+#define MIN_MODE_DECIMAL_FLOAT 1
+
+#define MAX_MODE_COMPLEX_FLOAT 2
+#define MIN_MODE_COMPLEX_FLOAT 1
+
+struct cl_optimization  
+{
+};
+
+typedef int  intptr_t ;
+
+#define TREE_LIST_CHECK
+#define BLOCK_CHECK
+enum tree_code2
+  {
+
+
+  };
+
+  
+
+
+//#define REAL_VALUE_TYPE_SIZE 32
+
+#define HAVE_STRING_H
+#define HAVE_STRSIGNAL
+#define HAVE_DECL_BASENAME 1
+#define CALL_EXPR_CHECK
+#define  GATHER_STATISTICS 0
+#include "system.h"
+#include "coretypes.h"
+
+#define INTEGER_CST_CHECK
+struct cl_target_option 
+{
+};
+#include <sys/types.h>
+#include "tree.h"
+
+// from ./gcc/c-family/c-opts.c
+//#include "system.h"
+//typedef int bool;
+
+
+
+enum machine_mode2{};
+//#define gcc_assert
+//CONST_CAST
+
+//#define ATTRIBUTE_FPTR_PRINTF(m, n)
+//#define ATTRIBUTE_PRINTF_2
+//#define ATTRIBUTE_PRINTF_3
+//#define ATTRIBUTE_PRINTF_5
+//#define ATTRIBUTE_UNUSED
+//#define HOST_WIDE_INT long long
+
+typedef __UINTPTR_TYPE__ uintptr_t;
+enum gt_types_enum 
+  {
+
+}
+  ;
+
